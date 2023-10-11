3eb52d1f1709b2d8f74cea00e4aab61bb381a664
Leonardo <leonardoww26@gmail.com>
2023-10-10 22:02:33 -0300

    Aula 10/10/23 certo

--- backend/src/guards/auth.guard.ts
+++ backend/src/guards/auth.guard.ts
@@ -1,11 +1,13 @@
 import {CanActivate, ExecutionContext, UnauthorizedException} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { UserService } from 'src/services/user.service';
 
 export class AuthGurd implements CanActivate {
 
     constructor(
-      private readonly jwtService: JwtService  
+      private readonly jwtService: JwtService,
+      private readonly userService: UserService
     ){}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -16,10 +18,19 @@ export class AuthGurd implements CanActivate {
             throw new UnauthorizedException('Token inexistente');
         }
 
+        let username = '';
         try {
             const payload = await this.jwtService.verifyAsync(token)
+            console.log('Buuuuuuuuuu', payload)
         } catch {
             throw new UnauthorizedException('Token Invalido')
+            
+        }
+
+        const found = await this.userService.findByUsername(username);
+
+        if(!found) {
+            throw new UnauthorizedException('Usuario não cadastrado')
         }
 
         return true;
